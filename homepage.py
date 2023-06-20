331848561a8a756abb7aedbcebe2e176babf4157
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 12:08:38 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title = "Telecom Homepage", page_icon=":📡:", layout ='wide')
 
+#-----------Header Section----------------------
+
+st.subheader("Hi, I am Parag Kar")
+
