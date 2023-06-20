ded12332e9f27d49b1696088cc480689b466661a
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 13:43:01 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -6,8 +6,8 @@ st.set_page_config(page_title = "Telecom Homepage", page_icon=":📡:", layout =
 
 st.title("An Integrated Application For The Telecom Industry")
 st.subheader("Hi, I am Parag Kar :🙏:")
-st.write("I wrote this app to unlock the story about the telecom industry")
+st.write("I wrote this app to tell the story about the telecom industry by visulalizing Data")
 st.write("The app is structured in four dimensions - 1) Spectrum Bands; 2) Auction Years; 3) Business Data; 4) Auction Data")
-st.write("")
+st.write("Each of these dimensions has features and options, which enables users to naviage deeper into the finner details of this story")
 st.write("[Link to the App>](https://paragkar-spectrummaps.streamlit.app/)")
 
