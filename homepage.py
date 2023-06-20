8dc440090e80dc583174c546b53e49a54abe508e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 13:55:36 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -23,5 +23,5 @@ with st.container():
 		st.write("##")
 		st.write(
 			"""
-			The Dimension "Spectrum Bands" has 3 Features - 1)Spectrum Map; 2)Expiry Map; 3)Auction Map
+			The Dim "Spectrum Bands" has 3 Features - 1)Spectrum Map; 2)Expiry Map; 3)Auction Map
 			""")
