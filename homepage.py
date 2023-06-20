05ab102e0a9cfd0dcca1665af2df2befb292d6aa
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 12:05:51 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title = "Telecom Homepage", page_icon="::")
+st.set_page_config(page_title = "Telecom Homepage", page_icon=":📡:")
 
-st.write("hello")
