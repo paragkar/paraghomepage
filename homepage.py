ef245c2398ac01e6107cced7d893628fbdc97980
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 14:00:07 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -19,7 +19,6 @@ with st.container():
 	st.write("---")
 	left_column, right_column = st.columns(2)
 	with left_column:
-		st.header("How this app works")
-		st.write("##")
+		st.header("How is this app structured")
 		st.write("The Dim 'Spectrum Bands' has 3 Features - 1)Spectrum Map; 2)Expiry Map; 3)Auction Map")
 		st.write("The Feature 'Spectrum Map' has 3 Sub Features - 1)Freq Layout; 2)Operator Holdings; 3) Operator %Share")
