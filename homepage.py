a7da0dbf7cca8218a1f7c74a3cb319a82e7ba00c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-20 13:27:44 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -5,5 +5,7 @@ st.set_page_config(page_title = "Telecom Homepage", page_icon=":📡:", layout =
 #-----------Header Section----------------------
 
 st.subheader("Hi, I am Parag Kar :🙏:")
-st.title("A Telecom Industry and Data Visulization Enthuisiast From India")
+st.title("I have Expecience in the Telecom Industry and Enthuisiast")
+st.write("I am passionate about using various visulaization techniques to unlock the story through data")
+st.write("[Link to the App >](https://paragkar-spectrummaps.streamlit.app/")
 
