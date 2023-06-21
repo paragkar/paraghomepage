2f06f581cd86ddd4fe3d475189dd08db188d9ba6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-21 10:53:49 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -100,22 +100,18 @@ with st.container():
 	st.header("Please Provide Your Valuable Feedback")
 	st.write("##")
 
-	contact_form = """
-	<form action="https://formsubmit.co/pkar@outlook.com" method="POST">
-	<input type = "hidden" name ="_captcha" value ="false">
-     <input type="text" name="name" placeholder = "Your name" required>
-     <input type="email" name="email" placeholder = "Your email" required>
-     <textarea name = "message" placeholder = "Yoour message here" required></textarea>
-     <button type="submit">Send</button>
-	</form>
-
-	"""
-
-	left_column, right_column = st.columns(2)
-	with left_column:
-		st.markdown(contact_form, unsafe_allow_html = True)
-	with right_column:
-		st.empty()
-
-
-
+	 # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/PKAR@OUTLOOK.COM" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
