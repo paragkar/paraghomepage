3efb6521b99e6e8eb8a50fc1d22e30b1535c2e55
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-21 10:51:26 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -19,7 +19,7 @@ def load_lottieurl(url):
 
 def local_css(file_name):
 	with open(file_name) as f:
-		st.markdown(f"<style>{f.read}</style>")
+		st.markdown(f"<style>{f.read}</style>", unsafe_allow_html = True)
 
 local_css("style/style.css")
 
