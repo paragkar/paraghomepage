28f39cba58466867d6ce4e04b1bd42aac1c03957
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-06-21 10:55:52 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -110,8 +110,8 @@ with st.container():
 	<button type="submit">Send</button>
 	</form>
 	"""
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.markdown(contact_form, unsafe_allow_html=True)
-    with right_column:
-        st.empty()
+	left_column, right_column = st.columns(2)
+	with left_column:
+		st.markdown(contact_form, unsafe_allow_html=True)
+	with right_column:
+		st.empty()
